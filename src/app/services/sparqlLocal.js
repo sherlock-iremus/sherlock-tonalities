6589538e222cfc4f76ec8cec39a68e5e74d96fe9
Félix <fpoullet@etu.utc.fr>
2022-03-10 17:15:47 +0100

    query beat notes

--- src/app/services/sparqlLocal.js
+++ src/app/services/sparqlLocal.js
@@ -1,19 +1,27 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { getNotesOnFirstBeat } from "./sparqlQueries";
 
 export const sparqlLocalEndpoint = createApi({
   reducerPath: "sparqlApi",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3030/tonalities" }),
   endpoints: (builder) => ({
-    sparql: builder.query({
-      query: (sparqlQuery) => ({
+    countTriples: builder.query({
+      query: () => ({
         url: "/sparql",
         method: "POST",
-        body: new URLSearchParams({ query: sparqlQuery }),
+        body: new URLSearchParams({ query: 'SELECT (COUNT(?s) AS ?triples) WHERE { ?s ?p ?o }' }),
       }),
     }),
+    getNotesOnFirstBeat: builder.query({
+      query: noteId => ({
+        url: "/sparql",
+        method: "POST",
+        body: new URLSearchParams({ query: getNotesOnFirstBeat(noteId) }),
+      }),
+    })
   }),
 });
 
 export default sparqlLocalEndpoint;
 
-export const { useSparqlQuery } = sparqlLocalEndpoint;
+export const { useCountTriplesQuery, useGetNotesOnFirstBeatQuery } = sparqlLocalEndpoint;
---
+++ src/app/services/sparqlQueries.js
@@ -0,0 +1,18 @@
+export const getNotesOnFirstBeat = noteId => `
+    PREFIX crm: <http://www.cidoc-crm.org/cidoc-crm/>
+    PREFIX sherlock: <http://data-iremus.huma-num.fr/ns/sherlockmei#>
+
+    SELECT ?notesInBeat
+    WHERE {
+        ?notesInBeat sherlock:contains_beats ?firstBeat.
+        {
+        SELECT ?firstBeat
+        WHERE {
+            ?selectedNote crm:P1_is_identified_by ?selectedNoteId.
+            FILTER(regex(STR(?selectedNote), "${noteId}$")).
+            ?selectedNote sherlock:contains_beats ?firstBeat
+        }
+        LIMIT 1
+        }
+    }
+`
