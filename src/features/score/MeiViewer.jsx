9f39017f3b611004d3139e483fd5b822072f349e
Félix <fpoullet@etu.utc.fr>
2022-04-28 15:26:37 +0200

    style and unstyle the score

--- src/features/score/MeiViewer.jsx
+++ src/features/score/MeiViewer.jsx
@@ -2,19 +2,40 @@
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { setInspectedNoteId } from '../inspection/inspectedEntitySlice'
-import { mainAreaStyle, verovioStyle } from '../meiviewer/mei.css'
-import { createVerovio, getNote, load } from '../meiviewer/verovioHelpers'
+import { verovioStyle } from '../meiviewer/mei.css'
+import { createVerovio, getNote, load, styleInspectedEntity } from '../meiviewer/verovioHelpers'
 
 window.verovioCallback = load
 
 export const MeiViewer = props => {
   useEffect(() => {
-    createVerovio(props.meiUrl) // github.com/rism-digital/verovio-app-react/blob/master/src/App.js
+    createVerovio(props.meiUrl)
   }, [])
 
   const dispatch = useDispatch()
 
   const isInspectionMode = useSelector(state => state.inspectedEntity.isInspectionMode)
+
+  const inspectedVerticalityId = useSelector(state => state.inspectedEntity.inspectedVerticalityId)
+  const inspectedNoteId = useSelector(state => state.inspectedEntity.inspectedNoteId)
+  const inspectedPositionnedNoteId = useSelector(state => state.inspectedEntity.inspectedPositionnedNoteId)
+  const inspectedSelectionId = useSelector(state => state.inspectedEntity.inspectedSelectionId)
+  const inspectedEntity =
+    inspectedNoteId || inspectedPositionnedNoteId || inspectedSelectionId || inspectedVerticalityId
+
+  const styleInspectedEntity = () => {
+    if (inspectedNoteId)
+      document.getElementById(inspectedNoteId.slice(props.scoreIri.length + 1)).classList.add('inspected')
+    // ...
+  }
+
+  const unStyleInspectedEntity = () => {
+    if (inspectedNoteId)
+      document.getElementById(inspectedNoteId.slice(props.scoreIri.length + 1)).classList.remove('inspected')
+    // ...
+  }
+
+  if (inspectedEntity) styleInspectedEntity()
 
   const handleMouseOver = e => {
     const n = getNote(e.target)
@@ -28,7 +49,10 @@ export const MeiViewer = props => {
 
   const handleClick = e => {
     const n = getNote(e.target)
-    n && isInspectionMode && dispatch(setInspectedNoteId(props.scoreIri + '_' + n.id))
+    if (n && isInspectionMode) {
+      if (inspectedEntity) unStyleInspectedEntity()
+      dispatch(setInspectedNoteId(props.scoreIri + '_' + n.id))
+    }
   }
 
   return (
