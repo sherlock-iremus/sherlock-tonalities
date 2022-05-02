b15c77571dc71ee719fe91a2d97fca2d4ad6b8dc
fpoullet <felix.poullet-pages@etu.utc.fr>
2022-05-02 11:42:42 +0200

    entities components go in inspector 2

--- src/features/score/inspector/NoteEntity.jsx
+++ src/features/score/inspector/NoteEntity.jsx
@@ -1,7 +1,7 @@
 import { MusicNote } from "@mui/icons-material"
 import { ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material"
-import { useGetNoteInfoQuery } from "../../app/services/sparql"
-import { LoadingNode } from "./LoadingNode"
+import { useGetNoteInfoQuery } from "../../../app/services/sparql"
+import { LoadingNode } from "./LoadingEntity"
 
 export const NoteNode = props => {
   const { data: noteLabel } = useGetNoteInfoQuery(props.note)
