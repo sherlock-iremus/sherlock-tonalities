b87dd01a683a05fbd8e3df8a6f019ff1fd7c150b
Félix <fpoullet@etu.utc.fr>
2022-05-23 14:23:47 +0200

    inspected selection is selected

--- src/features/score/navigator/Selections.jsx
+++ src/features/score/navigator/Selections.jsx
@@ -1,17 +1,19 @@
 import { List, ListItem, ListItemButton, ListItemText, ListSubheader } from '@mui/material'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { useGetScoreSelectionsQuery } from '../../../app/services/sparql'
 import { setInspectedSelection } from '../../slice/scoreSlice'
 
 export const Selections = props => {
   const { data: selections } = useGetScoreSelectionsQuery(props.scoreIri)
+  const { inspectedEntities, currentEntityIndex } = useSelector(state => state.score)
+  const { selectionIri: inspectedSelection } = inspectedEntities[currentEntityIndex]
   const dispatch = useDispatch()
 
   return (
     <List>
       {selections?.map(selection => (
         <ListItem key={selection.iri} disablePadding secondaryAction={props.secondaryAction}>
-          <ListItemButton onClick={() => dispatch(setInspectedSelection(selection.iri))}>
+          <ListItemButton onClick={() => dispatch(setInspectedSelection(selection.iri))} selected={selection.iri === inspectedSelection}>
             <ListItemText
               primary={`Selection with ${selection.entities} elements`}
               secondary={selection.iri.slice(props.baseUrl.length)}
