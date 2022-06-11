828a2e694aa44ced177a2b6795b515e587755151
felix-commits <fpoullet@mailo.com>
2022-06-12 01:39:13 +0200

    new verticality display

--- src/features/score/entities/VerticalityEntity.jsx
+++ src/features/score/entities/VerticalityEntity.jsx
@@ -1,18 +1,19 @@
-import { AlignHorizontalCenter, Close, Lyrics, Piano } from '@mui/icons-material'
+import { AlignHorizontalCenter, Close, ExpandLess, ExpandMore, Lyrics, Piano } from '@mui/icons-material'
 import {
+  Collapse,
   IconButton,
   List,
   ListItem,
   ListItemButton,
   ListItemIcon,
   ListItemText,
-  ListSubheader,
   SpeedDial,
   SpeedDialAction,
   SpeedDialIcon,
   Tooltip,
 } from '@mui/material'
 import { Box } from '@mui/system'
+import { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { useGetVerticalityPositionnedNotesQuery } from '../../../app/services/sparql'
 import { setInspectedEntity } from '../../slice/scoreSlice'
@@ -21,6 +22,7 @@ import { LoadingEntity } from './LoadingEntity'
 
 export const VerticalityEntity = ({ verticalityIri, baseUrl, clickedNoteIri }) => {
   const dispatch = useDispatch()
+  const [isOpen, setIsOpen] = useState(false)
   const { data: positionnedNotes } = useGetVerticalityPositionnedNotesQuery(verticalityIri)
   return positionnedNotes ? (
     <Box>
@@ -36,20 +38,25 @@ export const VerticalityEntity = ({ verticalityIri, baseUrl, clickedNoteIri }) =
           <ListItemIcon>
             <AlignHorizontalCenter />
           </ListItemIcon>
+          <Tooltip title="Positionned notes">
+            <IconButton onClick={() => setIsOpen(!isOpen)}>{isOpen ? <ExpandLess /> : <ExpandMore />}</IconButton>
+          </Tooltip>
           <ListItemText primary="Verticality" secondary={verticalityIri.slice(baseUrl.length)} />
         </ListItemButton>
       </ListItem>
-      <List subheader={<ListSubheader>Positionned notes on this verticality</ListSubheader>} dense disablePadding>
-        {positionnedNotes.map(e => (
-          <PositionnedNoteItem
-            key={e.positionnedNoteIri}
-            positionnedNoteIri={e.positionnedNoteIri}
-            attachedNoteIri={e.attachedNoteIri}
-            clickedNoteIri={clickedNoteIri}
-            baseUrl={baseUrl}
-          />
-        ))}
-      </List>
+      <Collapse in={isOpen} timeout="auto" unmountOnExit>
+        <List sx={{ pl: 2 }} dense disablePadding>
+          {positionnedNotes.map(e => (
+            <PositionnedNoteItem
+              key={e.positionnedNoteIri}
+              positionnedNoteIri={e.positionnedNoteIri}
+              attachedNoteIri={e.attachedNoteIri}
+              clickedNoteIri={clickedNoteIri}
+              baseUrl={baseUrl}
+            />
+          ))}
+        </List>
+      </Collapse>
 
       <Tooltip title="Create analytical entity">
         <SpeedDial ariaLabel="New" sx={{ position: 'fixed', bottom: 16, right: 16 }} icon={<SpeedDialIcon />}>
