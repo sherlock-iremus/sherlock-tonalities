8f64fffd0f408b8e35dfee07943f3bc8494854ba
Félix <fpoullet@etu.utc.fr>
2022-06-14 09:14:29 +0200

    add score to constants

--- src/features/meiviewer/constants.js
+++ src/features/meiviewer/constants.js
@@ -11,7 +11,8 @@ export const NOTE = 'http://data-iremus.huma-num.fr/id/d2a536eb-4a95-484f-b13d-f
 export const SELECTION = 'http://data-iremus.huma-num.fr/id/9d0388cb-a178-46b2-b047-b5a98f7bdf0b'
 export const POSITIONNED_NOTE = 'http://data-iremus.huma-num.fr/id/689e148d-a97d-45b4-898d-c395a24884df'
 export const VERTICALITY = 'http://data-iremus.huma-num.fr/id/90a2ae1e-0fbc-4357-ac8a-b4b3f2a06e86'
-export const ANNALYTICAL_ENTITY = 'http://data-iremus.huma-num.fr/id/6d72746a-9f28-4739-8786-c6415d53c56d'
+export const ANALYTICAL_ENTITY = 'http://data-iremus.huma-num.fr/id/6d72746a-9f28-4739-8786-c6415d53c56d'
+export const SCORE = 'http://data-iremus.huma-num.fr/sherlock/id/bf9dce29-8123-4e8e-b24d-0c7f134bbc8e'
 
 // STYLING MODES
 export const INSPECTED = 'inspected'
