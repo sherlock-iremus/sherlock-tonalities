7de4da21ca02274d41ed580f45140d8954fc878c
Félix <fpoullet@etu.utc.fr>
2022-06-14 09:13:26 +0200

    can post selections to sherlock

--- src/app/services/sherlockApi.js
+++ src/app/services/sherlockApi.js
@@ -2,15 +2,19 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const sherlockApi = createApi({
   reducerPath: 'sherlockApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://data-iremus.huma-num.fr/sherlock/api/', credentials: 'include' }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'http://data-iremus.huma-num.fr/sherlock/api/',
+  }),
   endpoints: builder => ({
     postSelection: builder.query({
-      query: () => ({
+      query: children => ({
         url: 'selection',
         method: 'POST',
-        body: new URLSearchParams({ children: [] }),
+        headers: {
+          Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJGw6lsaXggUG91bGxldC1QYWfDqHMiLCJuYmYiOjE2NTUxMjIwMDcsInJvbGVzIjpbXSwiaXNzIjoic2hlcmxvY2siLCJvcmNpZCI6IjAwMDAtMDAwMy0wNzQwLTc1MjciLCJleHAiOjE2NTUxMjU2MDcsImlhdCI6MTY1NTEyMjAwNywidXVpZCI6ImU0NzRjZDQzLTcwYzEtNGZhOC05MjNjLTRmZDJlNTYxNTgwNCJ9.q5tqN2wVcxswvZOaUtJh5tPFnRJO7qusk_8psCB4hyY`,
+        },
+        body: { children },
       }),
-      transformResponse: response => console.log(response),
     }),
   }),
 })
