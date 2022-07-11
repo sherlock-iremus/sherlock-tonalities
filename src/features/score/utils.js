91d3e924a087152bc496f519dd9a5e2cd076bcf8
Félix <fpoullet@etu.utc.fr>
2022-07-11 10:01:36 +0200

    add string to color fucntion

--- src/features/score/utils.js
+++ src/features/score/utils.js
@@ -28,3 +28,32 @@ export const findType = ({
   (scoreIri && SCORE)
 
 export const sleep = time => new Promise(resolve => setTimeout(resolve, time))
+
+const groupByOld = (list, key) => list.reduce((prev, curr) => ({ ...prev, [curr[key]]: [...(prev[key] || []), curr] }), {})
+
+export const groupBy = (list, key) => {
+  return list.reduce((prev, curr) => {
+    return {
+      ...prev,
+      [curr[key]]: [...(prev[key] || []), curr],
+    }
+  }, {})
+}
+
+export const stringToColor = string => {
+  let hash = 0
+  let i
+
+  /* eslint-disable no-bitwise */
+  for (i = 0; i < string.length; i += 1) hash = string.charCodeAt(i) + ((hash << 5) - hash)
+
+  let color = '#'
+
+  for (i = 0; i < 3; i += 1) {
+    const value = (hash >> (i * 8)) & 0xff
+    color += `00${value.toString(16)}`.slice(-2)
+  }
+  /* eslint-enable no-bitwise */
+
+  return color
+}
