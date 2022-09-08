fcb17672440d877421642c93ad3b539873bcf072
Thomas Bottini <thomas.bottini@cnrs.fr>
2022-09-08 14:14:05 +0200

    Guillotel

--- src/app/treatises/treatises.js
+++ src/app/treatises/treatises.js
@@ -2,15 +2,17 @@
 import Zarlino_1588 from './Zarlino_1588.json'
 import My_Treaty from './My_Treaty.json'
 import Proetorius_1619 from './Praetorius_1619.json'
+import Guillotel from './Guillotel.json'
 
 // include treatiseIris in array
-export const treatiseList = [Zarlino_1588.iri, My_Treaty.iri, Proetorius_1619.iri]
+export const treatiseList = [Zarlino_1588.iri, My_Treaty.iri, Proetorius_1619.iri, Guillotel]
 
 // include treatise in object
 const treatises = {
   [Zarlino_1588.iri]: Zarlino_1588,
   [My_Treaty.iri]: My_Treaty,
   [Proetorius_1619.iri]: Proetorius_1619,
+  [Guillotel.iri]: Guillotel,
 }
 
 export const getTreatise = treatiseIri => treatises[treatiseIri]
