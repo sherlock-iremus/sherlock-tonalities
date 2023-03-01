21c8839c8327fe08a49a146620d645c3c1f28bff
Félix <fpoullet@etu.utc.fr>
2023-03-01 16:36:55 +0100

    ui fix landing

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,30 +1,15 @@
-import { grey } from '@mui/material/colors'
-import { Box } from '@mui/system'
-import React from 'react'
 import { ScoreLibrary } from './features/ScoreLibrary'
+import { IconButton, Stack } from '@mui/material'
 import { ReactComponent as PolifoniaLogo } from './assets/polifonia.svg'
+import { grey } from '@mui/material/colors'
 import GitHubIcon from '@mui/icons-material/GitHub'
-import { IconButton } from '@mui/material'
 
-export const Landing = () => {
-  return (
-    <nav>
-      <Box
-        sx={{
-          height: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          bgcolor: grey[100],
-        }}
-      >
-        <PolifoniaLogo width={'100px'} height={'100px'} />
-        <ScoreLibrary />
-        <IconButton sx={{ marginTop: 2 }} href="https://github.com/sherlock-iremus/sherlock-tonalities">
-          <GitHubIcon />
-        </IconButton>
-      </Box>
-    </nav>
-  )
-}
+export const Landing = () => (
+  <Stack height="100vh" justifyContent="space-between" alignItems="center" bgcolor={grey[100]}>
+    <PolifoniaLogo style={{ alignSelf: 'start', padding: 16 }} width="150px" />
+    <ScoreLibrary />
+    <IconButton sx={{ alignSelf: 'end', padding: 2 }} href="https://github.com/sherlock-iremus/sherlock-tonalities">
+      <GitHubIcon />
+    </IconButton>
+  </Stack>
+)
