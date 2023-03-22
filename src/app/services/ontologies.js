a6c9609973040f680937226b3943114b4040faf5
Félix <fpoullet@etu.utc.fr>
2023-03-22 18:05:44 +0100

    #77 generate concept tree on rtk fetching

---
+++ src/app/services/ontologies.js
@@ -0,0 +1,36 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const ontologies = createApi({
+  reducerPath: 'ontologies',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://raw.githubusercontent.com/polifonia-project/modal-tonal-ontology/main/',
+  }),
+  endpoints: builder => ({
+    getCadencesGuillotel: builder.query({
+      query: () => ({ url: 'otherModels/JSON/cadences_Guillotel.json' }),
+      transformResponse: response => {
+        const classes = response.filter(node => node['@type'].includes('http://www.w3.org/2002/07/owl#Class'))
+        const classesIri = classes.map(c => c['@id'])
+        const classesWithParent = classes.map(c => {
+          const parent = c['http://www.w3.org/2000/01/rdf-schema#subClassOf']?.find(p =>
+            classesIri.includes(p['@id'])
+          )?.['@id']
+          return { iri: c['@id'], ...(parent && { parent }) }
+        })
+        const rootClasses = classesWithParent.filter(c => !c.parent)
+
+        const getSubClasses = iri => classesWithParent.filter(c => c.parent === iri).map(c => createNode(c.iri))
+
+        const createNode = iri => {
+          const subClasses = getSubClasses(iri)
+          return { iri, ...(subClasses.length && { subClasses }) }
+        }
+        return rootClasses.map(c => createNode(c.iri))
+      },
+    }),
+  }),
+})
+
+export default ontologies
+
+export const { useGetCadencesGuillotelQuery } = ontologies
--- src/app/store.js
+++ src/app/store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import scoreSlice from './services/scoreSlice'
 import sherlockApi from './services/sherlockApi'
 import sparqlApi from './services/sparql'
+import ontologies from './services/ontologies'
 import { tokenExpirationHandler } from './services/errorHandler'
 
 export const store = configureStore({
@@ -9,7 +10,12 @@ export const store = configureStore({
     score: scoreSlice.reducer,
     sherlockApi: sherlockApi.reducer,
     sparqlApi: sparqlApi.reducer,
+    ontologies: ontologies.reducer,
   },
   middleware: getDefaultMiddleware =>
-    getDefaultMiddleware().concat(sparqlApi.middleware).concat(sherlockApi.middleware).concat(tokenExpirationHandler),
+    getDefaultMiddleware()
+      .concat(sparqlApi.middleware)
+      .concat(sherlockApi.middleware)
+      .concat(ontologies.middleware)
+      .concat(tokenExpirationHandler),
 })
