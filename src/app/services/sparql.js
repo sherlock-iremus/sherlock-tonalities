24089b097e01d149e27f87d3c334e6e8ac1b0dd9
Félix <fpoullet@etu.utc.fr>
2023-04-05 17:31:49 +0200

    using yarn link with local sparql-querries repo

--- src/app/services/sparql.js
+++ src/app/services/sparql.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import { getContributor } from '../../features/items/ContributorItem'
 import { stringToColor } from '../../utils'
 import { BASE_API_URL } from './sherlockApi'
+import { getContributor } from 'sherlock-sparql-queries/src/queries/contributor'
 
 export const sparqlEndpoint = createApi({
   reducerPath: 'sparqlApi',
