cb534cfa164880153112e730d065678244ad722f
Félix <fpoullet@etu.utc.fr>
2023-04-05 13:28:50 +0200

    fix error if duplicate in selectednotes state

--- src/app/services/scoreSlice.js
+++ src/app/services/scoreSlice.js
@@ -26,9 +26,9 @@ const scoreSlice = createSlice({
     },
     setSelectedNotes: (state, action) => {
       if (!action.payload) state.selectedNotes = initialState.selectedNotes
-      else if (Array.isArray(action.payload)) {
-        state.selectedNotes.push(...action.payload)
-      } else {
+      else if (Array.isArray(action.payload))
+        state.selectedNotes.push(...action.payload.filter(e => !state.selectedNotes.includes(e)))
+      else {
         const index = state.selectedNotes.findIndex(e => e === action.payload)
         index !== -1 ? state.selectedNotes.splice(index, 1) : state.selectedNotes.push(action.payload)
       }
