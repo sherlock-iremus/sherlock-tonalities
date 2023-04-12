3a303eec6f35f482d5fc1f250f8aebf64e294b45
Félix <fpoullet@etu.utc.fr>
2023-04-12 18:35:49 +0200

    mv constants to queries repo

--- src/features/score/constants.js
+++
@@ -1,8 +0,0 @@
-export const NOTE = 'http://data-iremus.huma-num.fr/id/d2a536eb-4a95-484f-b13d-f597ac8ea2fd'
-export const SELECTION = 'http://data-iremus.huma-num.fr/id/9d0388cb-a178-46b2-b047-b5a98f7bdf0b'
-export const POSITIONNED_NOTE = 'http://data-iremus.huma-num.fr/id/689e148d-a97d-45b4-898d-c395a24884df'
-export const VERTICALITY = 'http://data-iremus.huma-num.fr/id/90a2ae1e-0fbc-4357-ac8a-b4b3f2a06e86'
-export const ANALYTICAL_ENTITY = 'http://data-iremus.huma-num.fr/id/6d72746a-9f28-4739-8786-c6415d53c56d'
-export const SCORE = 'http://data-iremus.huma-num.fr/id/bf9dce29-8123-4e8e-b24d-0c7f134bbc8e'
-export const SOFTWARE = 'http://data-iremus.huma-num.fr/id/29b00e39-75da-4945-b6c4-a0ca00f96f68'
-export const ANNOTATION = 'http://data-iremus.huma-num.fr/id/82dbd157-20df-422c-88db-28d6075d99a1'
