beec95204a049ab0b0e4557aa8a4cb3862a4b94a
Félix Poullet-Pagès <felix.poullet-pages@cnrs.fr>
2023-04-27 22:53:09 +0000

    ui to switch model

--- src/features/score/ContextMenu.jsx
+++ src/features/score/ContextMenu.jsx
@@ -1,5 +1,6 @@
 import { Menu } from '../../components/Menu'
 import { Concepts } from '../navigator/Concepts'
+import { List, ListItemButton, ListItemText } from '@mui/material'
 
 export const ContextMenu = ({ contextMenu, setContextMenu }) => (
   <Menu
@@ -8,6 +9,16 @@ export const ContextMenu = ({ contextMenu, setContextMenu }) => (
     anchorReference="anchorPosition"
     anchorPosition={contextMenu && { top: contextMenu.mouseY, left: contextMenu.mouseX }}
   >
-    <Concepts/>
+    <List dense disablePadding>
+      <ListItemButton disabled>
+        <ListItemText primary="Guillotel 2022" />
+      </ListItemButton>
+      <ListItemButton>
+        <ListItemText primary="Praetorius 1619" />
+      </ListItemButton>
+      <ListItemButton>
+        <ListItemText primary="Zarlino 1558" />
+      </ListItemButton>
+    </List>
   </Menu>
 )
