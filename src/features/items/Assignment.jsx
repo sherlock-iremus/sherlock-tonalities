eeeefc74df3c68f4a308cd0fa6f7b658d1c9adca
Félix <fpoullet@etu.utc.fr>
2023-07-12 11:28:27 +0200

    fix validate dom string

--- src/features/items/Assignment.jsx
+++ src/features/items/Assignment.jsx
@@ -30,52 +30,53 @@ export const Assignment = ({ assignment, concept, comment, refetch, date, author
   }
 
   return (
-    <Stack
-      key={concept || comment}
-      flex={1}
-      borderRadius={3}
-      bgcolor="secondary.main"
-      boxShadow={1}
-      overflow="hidden"
-      margin={1}
-    >
-      <ListItem
-        disablePadding
-        sx={{ '& .MuiListItemSecondaryAction-root': { top: 30 } }}
-
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-        secondaryAction={
-          <Collapse in={isHovered} timeout="auto" unmountOnExit>
-            <Stack direction="row" alignItems="center">
-              <ContributorItem contributorIri={author} />
-              <Tooltip title="Delete assignment">
-                <IconButton onClick={removeAssignment} size="small">
-                  {isLoading ? <CircularProgress /> : <Delete />}
-                </IconButton>
-              </Tooltip>
-            </Stack>
-          </Collapse>
-        }
+    <ul style={{ margin: 0, padding: 0 }}>
+      <Stack
+        key={concept || comment}
+        flex={1}
+        borderRadius={3}
+        bgcolor="secondary.main"
+        boxShadow={1}
+        overflow="hidden"
+        margin={1}
       >
-        <ListItemButton dense>
-          <ListItemIcon>
-            {concept ? (
-              <Tooltip title={getModel(concept) + ' music model'}>
-                <Avatar sx={{ height: 32, width: 32 }}>{getModel(concept)[0].toUpperCase()}</Avatar>
-              </Tooltip>
-            ) : (
-              <Avatar sx={{ height: 32, width: 32 }}>
-                <Comment fontSize="8" />
-              </Avatar>
-            )}
-          </ListItemIcon>
-          <ListItemText
-            primary={comment ? comment[0].toUpperCase() + comment : removeBaseIri(concept)}
-            secondary={new Date(date).toLocaleDateString('en-GB')}
-          ></ListItemText>
-        </ListItemButton>
-      </ListItem>
-    </Stack>
+        <ListItem
+          disablePadding
+          sx={{ '& .MuiListItemSecondaryAction-root': { top: 30 } }}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+          secondaryAction={
+            <Collapse in={isHovered} timeout="auto" unmountOnExit>
+              <Stack direction="row" alignItems="center">
+                <ContributorItem contributorIri={author} />
+                <Tooltip title="Delete assignment">
+                  <IconButton onClick={removeAssignment} size="small">
+                    {isLoading ? <CircularProgress /> : <Delete />}
+                  </IconButton>
+                </Tooltip>
+              </Stack>
+            </Collapse>
+          }
+        >
+          <ListItemButton dense>
+            <ListItemIcon>
+              {concept ? (
+                <Tooltip title={getModel(concept) + ' music model'}>
+                  <Avatar sx={{ height: 32, width: 32 }}>{getModel(concept)[0].toUpperCase()}</Avatar>
+                </Tooltip>
+              ) : (
+                <Avatar sx={{ height: 32, width: 32 }}>
+                  <Comment fontSize="8" />
+                </Avatar>
+              )}
+            </ListItemIcon>
+            <ListItemText
+              primary={comment ? comment[0].toUpperCase() + comment : removeBaseIri(concept)}
+              secondary={new Date(date).toLocaleDateString('en-GB')}
+            ></ListItemText>
+          </ListItemButton>
+        </ListItem>
+      </Stack>
+    </ul>
   )
 }
