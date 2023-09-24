50b3b6f36c95565fe777aaa68644515b178ba7e6
Félix Poullet-Pagès <felix.poullet-pages@cnrs.fr>
2023-09-24 21:02:51 +0200

    update yarn

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: '/sherlock-tonalities',
+  base: '/sherlock-tonalities/',
   server: {
     open: true,
   },
