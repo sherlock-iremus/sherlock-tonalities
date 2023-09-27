1c0f9f217b3f96731150f961a08a04dc23f6a9b7
Félix Poullet-Pagès <felix.poullet-pages@cnrs.fr>
2023-09-27 11:18:44 +0200

    update base url

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: '/sherlock-tonalities/',
+  base: '/tonalities/',
   server: {
     open: true,
   },
