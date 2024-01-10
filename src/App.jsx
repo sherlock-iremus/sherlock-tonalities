2d509a8f78fe65124c731c8f17a9fc619eebd0fc
Félix Poullet-Pagès <felix.poullet-pages@cnrs.fr>
2024-01-10 21:25:28 +0100

    update react router v6

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,32 @@
 import { useGetUserIdQuery } from './services/service'
-import { Route, Routes } from 'react-router-dom'
 import { ScoreAnnotator } from './features/score/ScoreAnnotator'
 import { useMemo } from 'react'
 import { Landing } from './features/Landing'
 import { ThemeProvider, createTheme } from '@mui/material'
 import { colors } from './features/ThemePicker'
 import { useSelector } from 'react-redux'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter(
+  [
+    { path: '*', element: <Landing />  },
+    { path: '/project/:projectId/score/:scoreId', element: <ScoreAnnotator /> },
+  ],
+  {
+    basename: '/tonalities',
+  }
+)
 
 export const App = () => {
   useGetUserIdQuery()
   const { colorIndex } = useSelector(state => state.globals)
-
   const theme = useMemo(
     () => createTheme({ palette: { primary: colors[colorIndex], secondary: { main: colors[colorIndex][50] } } }),
     [colorIndex]
   )
-
   return (
     <ThemeProvider {...{ theme }}>
-      <Routes>
-        <Route path="/project/:projectId/score/:scoreId" element={<ScoreAnnotator />} />
-        <Route path="*" element={<Landing />} />
-      </Routes>
+      <RouterProvider router={router} />
     </ThemeProvider>
   )
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { HashRouter } from 'react-router-dom'
 import { store } from './services/store'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
@@ -10,11 +9,9 @@ verovio.module.onRuntimeInitialized = () => {
   window.tk = new verovio.toolkit()
   createRoot(document.getElementById('app')).render(
     <React.StrictMode>
-      <HashRouter>
-        <Provider {...{ store }}>
-          <App />
-        </Provider>
-      </HashRouter>
+      <Provider {...{ store }}>
+        <App />
+      </Provider>
     </React.StrictMode>
   )
 }
