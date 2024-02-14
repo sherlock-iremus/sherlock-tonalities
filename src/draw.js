3e7caa2e6554c98a4f3f3d694419ef09c8b7786a
Félix Poullet-Pagès <felix.poullet-pages@cnrs.fr>
2024-02-14 14:25:12 +0100

    findInBetweenNotes 2

--- src/draw.js
+++ src/draw.js
@@ -26,15 +26,3 @@ const staffCoords = staff => ({
   top: staff.getBBox().y,
   bottom: staff.getBBox().y + staff.getBBox().height,
 })
-
-export const findInBetweenNotes = (initialNoteId, finalNoteId) => {
-  const initialNote = document.getElementById(initialNoteId)
-  const finalNote = document.getElementById(finalNoteId)
-  const yMin = Math.min(staffCoords(getLayer(initialNote)).top, staffCoords(getLayer(finalNote)).top)
-  const yMax = Math.max(staffCoords(getLayer(initialNote)).bottom, staffCoords(getLayer(finalNote)).bottom)
-  const xMin = Math.min(noteCoords(initialNote)[0], noteCoords(finalNote)[0])
-  const xMax = Math.max(noteCoords(initialNote)[0], noteCoords(finalNote)[0])
-  const notes = [...document.querySelectorAll('.note')].map(note => ({ id: note.id, coords: noteCoords(note) }))
-  const inBetweenNotes = notes.filter(({ coords: [x, y] }) => x > xMin && x < xMax && y > yMin && y < yMax)
-  return [finalNoteId, ...inBetweenNotes.map(note => note.id)]
-}
