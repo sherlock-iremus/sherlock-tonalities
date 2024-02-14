ea1ad83151e140180bc1783d6e36781ea6330cc7
Félix Poullet-Pagès <felix.poullet-pages@cnrs.fr>
2024-02-14 14:24:21 +0100

    findInbetweenNotes

--- src/features/score/MeiViewer.jsx
+++ src/features/score/MeiViewer.jsx
@@ -14,7 +14,7 @@ import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { setNoteCount, setSelectedNotes } from '../../services/globals'
-import { circleShape, findInBetweenNotes, noteCoords } from '../../draw'
+import { circleShape, noteCoords } from '../../draw'
 import { AccountMenu } from '../AccountMenu'
 import { verovioStyle } from './style'
 import { Editor } from '../edition/Editor'
@@ -95,6 +95,18 @@ export const MeiViewer = ({ file }) => {
       e => e.time.realTimeOnsetMilliseconds <= time && time < e.time.realTimeOffsetMilliseconds
     )
     dispatch(setSelectedNotes(filteredNotes.map(e => e.id)))
+  }
+
+  const findInBetweenNotes = (initialNoteId, finalNoteId) => {
+    const notes = [...document.querySelectorAll('.note, .rest, .mRest')]
+    const notesOffset = notes.map(e => ({
+      id: e.id,
+      offset: toolkit.getTimeForElement(e.id),
+    }))
+    const initialOffset = notesOffset.find(note => note.id === initialNoteId).offset
+    const finalOffset = notesOffset.find(note => note.id === finalNoteId).offset
+    const inBetweenNotes = notesOffset.filter(e => e.offset >= initialOffset && e.offset <= finalOffset)
+    return inBetweenNotes.map(note => note.id)
   }
 
   const changePage = newPage => {
