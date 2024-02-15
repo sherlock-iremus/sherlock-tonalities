612d37dd6537bae3895b3c64b6e18769f56cc428
Félix Poullet-Pagès <felix.poullet-pages@cnrs.fr>
2024-02-15 16:36:27 +0100

    #150

--- src/features/score/VirtualizedList.jsx
+++ src/features/score/VirtualizedList.jsx
@@ -1,5 +1,13 @@
 import { AudioFile, ChevronRight } from '@mui/icons-material'
-import { IconButton, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
+import {
+  IconButton,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  ListSubheader,
+  Typography,
+} from '@mui/material'
 import { FixedSizeList as List } from 'react-window'
 
 export const VirtualizedList = ({ scores, selectedComposers, filter, selectedScoreId, setSelectedScoreId }) => {
@@ -35,8 +43,13 @@ export const VirtualizedList = ({ scores, selectedComposers, filter, selectedSco
   }
 
   return (
-    <List height={400} itemCount={filteredScores.length} itemSize={60} width="100%">
-      {Row}
-    </List>
+    <>
+      <Typography variant="caption" textAlign="end" px={1} color="text.secondary">
+        {filteredScores.length} results
+      </Typography>
+      <List height={400} itemCount={filteredScores.length} itemSize={60} width="100%">
+        {Row}
+      </List>
+    </>
   )
 }
