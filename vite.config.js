e888c6b0682207984a59ee03e52feb8fc9e26da3
Félix Poullet-Pagès <felix.poullet-pages@cnrs.fr>
2024-04-21 16:40:05 +0200

    deploy to gh pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: '/tonalities/',
+  base: '/sherlock-tonalities/',
   server: {
     open: true,
   },
